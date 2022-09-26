78311aa185d7f2531627b05e2917ed2c0e774deb
Luke Wong <lukewong93@gmail.com>
2022-09-26 14:11:31 +0800

    refactor: create a theme file

---
+++ src/theme.js
@@ -0,0 +1,21 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      light: "ff9b3f",
+      main: "#ff6a00",
+      dark: "c43800",
+    },
+    secondary: {
+      light: "#4f5b62",
+      main: "#263238",
+      dark: "#000a12",
+    },
+    tertiary: {
+      light: "#ffffff",
+      main: "#f5f5f5",
+      dark: "#c2c2c2",
+    },
+  },
+});
