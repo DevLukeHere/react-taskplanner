f949b6d7b246fb0ba0facae0a7f92b67c8853db3
Luke Wong <lukewong93@gmail.com>
2022-09-26 23:31:20 +0800

    refactor: create filter component

---
+++ src/components/Filters.jsx
@@ -0,0 +1,37 @@
+import {
+  Box,
+  Typography,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  FormControl,
+} from "@mui/material";
+import React, { useState } from "react";
+
+export default function Filters() {
+  return (
+    <Box>
+      <Typography variant="subtitle1">Filter by:</Typography>
+      <FormControl>
+        <RadioGroup
+          sx={{ flexDirection: "row" }}
+          aria-labelledby="task-status-group"
+          defaultValue="inProgress"
+          name="status-buttons-group"
+        >
+          <FormControlLabel
+            value="inProgress"
+            control={<Radio />}
+            label="In Progress"
+          />
+          <FormControlLabel value="done" control={<Radio />} label="Done" />
+          <FormControlLabel
+            value="complete"
+            control={<Radio />}
+            label="Complete"
+          />
+        </RadioGroup>
+      </FormControl>
+    </Box>
+  );
+}
